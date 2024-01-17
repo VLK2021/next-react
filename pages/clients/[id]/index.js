6eb9829760092952cf06c06b031b71dace261649
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-01-18 00:18:00 +0200

    routing and 404

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {useRouter} from "next/router";
 
+
 const ClientsProjectPage = () => {
     const router = useRouter();
 
     console.log(router.query);
 
+    function loadProjectHandler() {
+//..load data
+
+        router.push({
+            pathname: '/clients/[id]/[clienntprojectid]',
+            query: {id: 'max', clienntprojectid: 'projeta'}
+        });
+
+    }
+
 
     return (
         <div>
             <h1>ClientsProjectPage</h1>
+
+            <button onClick={loadProjectHandler}>Load project A</button>
         </div>
     );
 };
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,8 +1,32 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 
 const ClientsPage = () => {
+    const router = useRouter();
+    console.log(router.query);
+
+    const client = [
+        {id: 'max', name: "Maximilian"},
+        {id: "manu", name: "Manuel"}
+    ];
+
+
     return (
         <div>
             <h1>ClientsPage</h1>
+
+            <ul>
+                {
+                    client.map(obj =>
+                        <li key={obj.id}>
+                            <Link href={{
+                                pathname: '/clients/[id]',
+                                query: {id: obj.id}
+                            }}>{obj.name}</Link>
+                        </li>)
+                }
+            </ul>
         </div>
     );
 };
