2ac78583a92834a3c653160c34e67749b988c32f
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-01-20 22:57:55 +0200

    commit

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
-import Link from "next/link";
-
 import classes from './event-item.module.css';
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icons";
 
 
 const EventItem = (props) => {
@@ -21,20 +23,30 @@ const EventItem = (props) => {
             <img src={'/' + image} alt={title} />
 
             <div className={classes.content}>
-                <div className={classes.smmary}>
+                <div className={classes.summary}>
                     <h2>{title}</h2>
 
                     <div className={classes.date}>
+                        <DateIcon/>
+
                         <time>{humanReadableDate}</time>
                     </div>
 
                     <div className={classes.address}>
+                        <AddressIcon/>
+
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
 
                 <div className={classes.actions}>
-                    <Link href={exploreLink}>Explore Event</Link>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+
+                        <span className={classes.icon}>
+                            <ArrowRightIcon/>
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
