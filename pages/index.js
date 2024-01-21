2774f92f1622a33391cc7860622f3fe1e4047fc8
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-01-21 16:05:34 +0200

    start

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
-import {getFeaturedEvents} from "../dummy-data";
-
-import EventList from "../components/events/event-list";
-
-
 const Home = () => {
-    const featuredEvents = getFeaturedEvents();
-
 
     return (
         <main>
-            <EventList items={featuredEvents}/>
+            NEXT
         </main>
     )
 }
