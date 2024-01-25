8626a8d26ca25445c5c78c40545d73bf8aaeb63d
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-01-25 19:18:37 +0200

    start project

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import EventList from "../components/events/event-list";
-import {getFeaturedEvents} from "../helpers/api-util";
+const Home = () => {
 
-
-export async function getStaticProps() {
-    const filterTransformSales = await getFeaturedEvents();
-
-    return {
-        props: {
-            featuredEvents: filterTransformSales
-        },
-        revalidate: 1800
-    }
-}
-
-
-const Home = (props) => {
-    const {featuredEvents} = props;
 
     return (
         <main>
-            <EventList items={featuredEvents}/>
+            NEXT
         </main>
     )
 }
